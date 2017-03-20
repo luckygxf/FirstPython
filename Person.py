1115022301884f22d802007f65e781baaccff06d
luckygxf <403261934@qq.com>
2017-03-20 23:43:25 +0800

    second commit

--- Person.py
+++ Person.py
@@ -1,8 +1,13 @@
 # -*- coding=utf-8 -*-
-#define Person class
+# define Person class
 import requests
 import codecs
 import bs4
+import urllib2
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 class Person:
     def __init__(self, name, age):
@@ -11,13 +16,15 @@ class Person:
 
 #爬取豆瓣书单
 def getBookList():
-    #url = 'https://book.douban.com/tag/%E6%8E%A8%E7%90%86'; #推理小说列表
-    #response = requests.get(url)                            #加载要解析的网页
-    #soup = bs4.BeautifulSoup(response.text)                 #获取beautifulSoup对象
+    url = 'https://book.douban.com/tag/推理'; #推理小说列表
+    response = requests.get(url)                            #加载要解析的网页
+    soup = bs4.BeautifulSoup(response.content, 'html.parser')                 #获取beautifulSoup对象
 
-    soup = bs4.BeautifulSoup(codecs.open('content.txt', 'r', 'utf-8'))        #获取beautifulSoup对象
+    # soup = bs4.BeautifulSoup(codecs.open('content.txt', 'r', 'utf-8'), 'html.parser')        #获取beautifulSoup对象
     links = soup.select('div.info a')                       #利用soup选择器获取书名连接
     print 'length of links = %d\n' %len(links)
+    saveContentToTxtFile(response.content, 'content.txt')
+    # saveContentToTxtFile(response.text, 'txt.txt')
 
     for a in links:
             if((a is not None)&(a.string is not None)):
@@ -29,6 +36,12 @@ def saveContentToTxtFile(content, filename):
     f.write(content)
     f.close()
 
+# 使用urllib2获取网页内容
+def getHtmlUseUrllib2():
+    url = 'https://book.douban.com/tag/推理';  # 推理小说列表
+    response = urllib2.urlopen(url)
+    html = response.read()
+    saveContentToTxtFile(html, 'content_urllib2.txt')
 
 if __name__ == '__main__':
     #saveContentToTxtFile('welcome ', 'test.txt')
